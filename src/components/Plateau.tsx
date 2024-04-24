0ce0257e89ed36b95010c593cf1fe1cc48260c77
julienjamet <j.jamet@alphalink.fr>
2024-04-25 01:20:07 +0200

    final places

--- src/components/Plateau.tsx
+++ src/components/Plateau.tsx
@@ -26,23 +26,25 @@ export const Plateau: FC = () => {
         }
     }
 
+    // USED PLACES
     const used: number[] = [
         2, 3, 4, 5, 6, 7, 11, 12, 13,
         29, 35, 39, 42,
-        57, 63, 64, 65, 66, 67, 70,
-        85, 86, 87, 98,
-        115, 126,
-        143, 154,
+        57, 60, 63, 64, 65, 66, 67, 70,
+        85, 86, 87, 88, 93, 94, 95, 96, 98,
+        115, 116, 126,
+        143, 144, 154,
         171, 180, 181, 182,
         197, 198, 199, 208,
-        225, 236,
-        253, 264,
-        281, 292, 293, 294,
-        309, 312, 313, 314, 315, 316, 322,
+        225, 235, 236,
+        253, 263, 264,
+        281, 283, 284, 285, 286, 291, 292, 293, 294,
+        309, 312, 313, 314, 315, 316, 319, 322,
         337, 340, 344, 350,
         366, 367, 368, 372, 373, 374, 375, 376, 377
     ];
 
+    // FOUR MARKS
     const fourMarks: number[] = [
         3, 7, 11,
         70,
@@ -52,6 +54,20 @@ export const Plateau: FC = () => {
         294,
         309,
         368, 372, 376
+    ];
+
+    // FINAL PLACES
+    const finalBlue: number[] = [
+        283, 284, 285, 286
+    ];
+    const finalGreen: number[] = [
+        93, 94, 95, 96
+    ];
+    const finalYellow: number[] = [
+        60, 88, 116, 144
+    ];
+    const finalRed: number[] = [
+        235, 263, 291, 319
     ];
     /****************************************************/
 
@@ -67,6 +83,10 @@ export const Plateau: FC = () => {
                                     `spot
                                     ${used.includes(div) ? 'used' : ''}
                                     ${fourMarks.includes(div) ? 'four' : ''}
+                                    ${finalRed.includes(div) ? 'finalRed' : ''}
+                                    ${finalBlue.includes(div) ? 'finalBlue' : ''}
+                                    ${finalGreen.includes(div) ? 'finalGreen' : ''}
+                                    ${finalYellow.includes(div) ? 'finalYellow' : ''}
                                     ${div === 65 ? 'outGreen' : div === 143 ? 'outYellow' : div === 236 ? 'outRed' : div === 314 ? 'outBlue' : ''}`
                                 }
                             >
