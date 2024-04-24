b3c2244a226baabcc5c2b369ae60495b68507289
julienjamet <j.jamet@alphalink.fr>
2024-04-24 19:21:54 +0200

    init project

---
+++ src/interfaces.ts
@@ -0,0 +1,33 @@
+export interface ISpeed {
+    speed: number;
+    setSpeed: (speed: number) => void;
+};
+
+export interface IBlackCount {
+    blackCount: number;
+    setBlackCount: (blackCount: number) => void;
+};
+
+export interface IMovesCount {
+    movesCount: number;
+};
+
+export interface ILaunch {
+    setLaunch: (launch: boolean) => void;
+};
+
+export interface IOver {
+    over: boolean;
+    setBlack: (black: number[]) => void;
+    setBlackCount: (blackCount: number) => void;
+    setActive: (active: number) => void;
+    setLaunch: (launch: boolean) => void;
+    setMovesCount: (movesCount: number) => void;
+    setOver: (over: boolean) => void;
+};
+
+export interface IMatrice {
+    active: number;
+    setActive: (active: number) => void;
+    used: number[];
+};
